e77997cebc25532b022cc842124b0f91510e1b4f
AugustineKadima <kadimasam@gmail.com>
2022-08-24 15:02:59 +0300

    Add diary form

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,6 +2,27 @@ import React from "react";
 
 export default function Dashboard(){
     return (
-        <div></div>
+        <div className="dashboard_container">
+
+            <div className="dashboard_wrapper">
+            <form>
+                <h2>New Diary Item</h2>
+      
+                <div>
+                    <label>Title</label>
+                    <input placeholder="Enter Item Title" name="title" type="text"/>
+                </div>
+          
+                <div>
+                    <label>Description</label>
+                    <textarea placeholder="Enter Description" name="description" type="text"/>
+                </div>
+            
+                <button>Add To Diary</button>
+            
+            </form>
+            </div>
+
+        </div>
     )
 }
