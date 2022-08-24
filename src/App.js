63cb07f4282b190cab056aad268461b212e433ac
AugustineKadima <kadimasam@gmail.com>
2022-08-24 09:14:41 +0300

    Add landing page design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/navbar';
+import LandingPage from './components/landing';
+import Login from './components/login';
+import SignUp from './components/signup';
+import Dashboard from './components/dashboard';
 
 function App() {
   return (
     <div className='main_container'>
       <NavBar />
+      <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
      
     </div>
   );
--- src/components/landing.js
+++ src/components/landing.js
@@ -1,9 +1,22 @@
 import React from "react";
+import{ IoMdArrowForward} from 'react-icons/io'
 
 export default function LandingPage(){
     return (
-        <div>
-            
+        <div className="landing_container">
+            <div className="landing_content_wrapper">
+                <h1>Document Your Experiences, Keep Track Of Life Events And Worry Less About Your Memories Getting Lost</h1>
+                <p>Physical diaries wear out, get torn and sometimes get lost. We are saving you from these by providing 
+                an online diary that allows you to document experiences in a secure while their quality remains the same
+                </p>
+
+                <button id="get_started_button">Get Started &nbsp; <IoMdArrowForward /></button>
+
+            </div>
+            <div className="landing_image_wrapper">
+                <img src="../mediary_landing.png"/>
+            </div>
+
         </div>
     )
 }
