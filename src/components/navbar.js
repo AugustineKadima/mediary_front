9c988e37a5c0ebac94db9147fda1aaf65f5976f9
AugustineKadima <kadimasam@gmail.com>
2022-08-24 13:42:44 +0300

    Add signup component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar(){
     return (
         <div className="navbar_container">
 
             <div className="navbar_wrapper">
-                <h2>Mediary</h2>
+                <NavLink to="/"><h2>Mediary</h2></NavLink>
                 <div className="nav_buttons_wrapper">
-                    <button id="signup_btn">Sign Up</button>
+                   <NavLink to="/signup"> <button id="signup_btn">Sign Up</button></NavLink>
                     <button id="login_btn">Login</button>
                 </div>
             </div>
--- src/components/signup.js
+++ src/components/signup.js
@@ -2,6 +2,37 @@ import React from "react";
 
 export default function SignUp(){
     return (
-        <div></div>
+        <div className="sign_up_container">
+            <form>
+                <h2>Sign Up</h2>
+                <div>
+                    <label>Name</label>
+                    <input placeholder="Enter Name" name="name" type="text"/>
+                </div>
+                <div>
+                    <label>Username</label>
+                    <input placeholder="Enter Username" name="username" type="text"/>
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input placeholder="Enter Email" name="email" type="email"/>
+                </div>
+                <div>
+                    <label>Age</label>
+                    <input placeholder="Enter Age" name="age" type="number"/>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input placeholder="Enter Password" name="password" type="password"/>
+                </div>
+                <div>
+                    <label>Confirm Password</label>
+                    <input placeholder="Confirm Password" name="confirm_password" type="password"/>
+                </div>
+
+                <button>Sign Up</button>
+            
+            </form>
+        </div>
     )
 }
